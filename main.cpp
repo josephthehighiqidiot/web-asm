f1ecbc595a255e90bb8fe704fa0f4362df454513
joseph <joeyryan8888@gmail.com>
2025-11-25 10:50:14 -0600

    e

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <limits>
 class class1 {
     public:
         void hello() {
@@ -7,11 +9,32 @@ class class1 {
         void test() {
             std::cout << "This is an example class." << std::endl;
         }
+        void test2() {
+            for (int i = 1; i < 10; i++)
+            {
+            std::cout << "testing" << i << std::endl;
+            }
+        }
+        void requestInput() {
+            std::string input;
+            std::cout << "type somthing:" << std::endl;
+            std::cin >> input;
+            std::cout << "You entered: " << input << std::endl;
+        }
 };
+
+void print(std::string string)
+{
+    std::cout << string << std::endl;
+}
 
 int main() {
     class1 class1;
+    std::string name;
     class1.hello();
     class1.test();
+    class1.test2();
+    class1.requestInput();
+    print("1");
     return 0;
 }
