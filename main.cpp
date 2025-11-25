a2e867d9fd3508c74e8ff9e9afac27be41aca689
Joseph Ryan <93546701+josephthehighiqidiot@users.noreply.github.com>
2025-11-25 16:13:18 +0000

    e

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+class Example {
+    public:
+        void display() {
+            std::cout << "This is an example class." << std::endl;
+        }
+}
 
+int main() {
+    Example ex;
+    std::cout << "Hello, World!" << std::endl;
+    ex.display();
+    return 0;
+}
